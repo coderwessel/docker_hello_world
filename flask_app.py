d7e2dbda9ca27f2f16678c9682e44b79906d4d53
W.Kieft <w.kieft@uva.nl>
2024-05-10 16:03:20 +0200

    hmm why it doesnt work yet

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-        return("HEllo there yes!")
+        return 'HEllo there yes!'
 
 if __name__=="__main__":
     app.run(debug=True)
